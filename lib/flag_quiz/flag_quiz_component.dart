e5bd058443d1ce75c146335d07bc1b102d974e16
Patrick Gallagher <patrickj@cpgallagher.com>
2017-05-17 00:45:01 -0500

    Added a service to get country codes and populate with a new flag each time

--- lib/flag_quiz/flag_quiz_component.dart
+++ lib/flag_quiz/flag_quiz_component.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'package:FlagQuiz/flag_quiz/flag_quiz_service.dart';
 import 'package:angular2/core.dart';
 import 'package:angular_components/angular_components.dart';
 
@@ -5,15 +7,33 @@ import 'package:angular_components/angular_components.dart';
   selector: 'flag-quiz',
   styleUrls: const ['flag_quiz_component.css'],
   templateUrl: 'flag_quiz_component.html',
-  directives: const [materialDirectives]
+  directives: const [materialDirectives],
+  providers: const [CountryService]
 )
-class FlagQuizComponent {
+class FlagQuizComponent implements OnInit {
+  final CountryService _countryService;
   String guess = "";
   String flagUrl = "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/300px-Flag_of_the_United_States.svg.png";
+  String actualCountry = "";
   bool isButtonEnabled = true;
+
+  FlagQuizComponent(this._countryService);
+
+  @override
+  Future ngOnInit() async {
+    try {
+      flagUrl = 'https://flagpedia.net/data/flags/normal/${_countryService.getRandomCountryCode().toLowerCase()}.png';
+      print('Test');
+    } catch (err) {
+      actualCountry = "";
+      print('Error getting countries: $err');
+    }
+  }
 
   void submitGuess() {
     guess = guess + "!";
+    flagUrl = 'http://flags.fmcdn.net/data/flags/w580/${_countryService.getRandomCountryCode().toLowerCase()}.png';
+    print(flagUrl);
   }
 
   void updateGuess(String s) {
